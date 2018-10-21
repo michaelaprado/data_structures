3a56830b404571444b01bbefcf425c236098caea
Michaela Prado <michaelaprado98@gmail.com>
2018-10-21 01:52:26 -0500

    Add:get, find, addAt, deleteAdd

--- linked_list/main.py
+++ linked_list/main.py
@@ -50,12 +50,63 @@ class LinkedList:
                 a = a.next
             a.next = None 
             return
-
+    def get(self,indexn):
+        a = self.head
+        i = 0
+        while(i < indexn):
+            i = i+1
+            a = a.next
+            if (a == None):
+                return
+        return a.value
+    def find(self, valor):
+        a = self.head
+        if(a == None):
+            return -1
+        i = 0
+        while(a.value != valor):
+            i = i+1
+            a = a.next
+            if(a == None):
+                return -1
+        return i
+    def addAt(self, indexn, value):
+        new_node = Node(value)
+        a = self.head
+        i = 0
+        if (a == None):
+            print("no se puede añadir")
+            return
+        if(indexn == 0):
+           new_node.next = a
+           self.head = new_node
+           return
+        while(i < (indexn-1)):
+            i = i + 1
+            a = a.next
+            if(a == None):
+                print("no se puede añadir")
+                return 
+        new_node.next = a.next
+        a.next = new_node
+    def deleteAt(self, indexn):
+        a = self.head
+        i = 0
+        if(a == None):
+            print("no se puede borrar")
+            return
+        if(indexn == 0):
+            self.head = a.next
+            return 
+        while(i < (indexn-1)):
+            i = i+1
+            a = a.next
+            if (a.next == None):
+                print("no se puede borrar")
+                return
+        a.next = a.next.next
 lista = LinkedList()
-lista.add(1)
-lista.add(2)
-lista.add(3)
-lista.delete_last()
+lista.addAt(0, 6)
 lista.print_nodes()
 
 
